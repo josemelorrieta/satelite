9f51b41b824ca3d06f2e6389cd3595823c00f1a6
josemelorrieta <46441341+josemelorrieta@users.noreply.github.com>
2019-01-08 09:10:13 +0100

    Add files via upload

---
+++ src/main/java/Satelite.java
@@ -0,0 +1,26 @@
+
+public class Satelite {
+	public double meridiano;
+	public double paralelo;
+	public double distanciaTierra;
+	
+	public Satelite(double m,double p,double d) {
+		this.meridiano=m;
+		this.paralelo=p;
+		this.distanciaTierra=d;
+	}
+	
+	public Satelite() {
+		this.meridiano=paralelo=distanciaTierra=0;
+	}
+	
+	public void setPosicion(double m,double p,double d) {
+		this.meridiano=m;
+		this.paralelo=p;
+		this.distanciaTierra=d;
+	}
+	
+	public void printPosicion() {
+		System.out.println("El satélite se encuentra en el paralelo " + this.paralelo + " meridiano " + this.meridiano + " a una distancia de la tierra de " + this.distanciaTierra + " Kilómetros");
+	}
+}
---
+++ src/test/java/TestSatelite.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSatelite {
+	private double meridiano;
+	private double paralelo;
+	private double distanciaTierra;
+
+	@Test
+	public void testConstructor1() {
+		meridiano = 107;
+		paralelo = 200;
+		distanciaTierra = 500;
+		Satelite sateliteTest = new Satelite(meridiano, paralelo, distanciaTierra);
+		assertEquals(sateliteTest.meridiano, 107, 0);
+		assertEquals(sateliteTest.paralelo, 200, 0);
+		assertEquals(sateliteTest.distanciaTierra, 500, 0);
+	}
+
+	@Test
+	public void testConstructor2() {
+		Satelite sateliteTest = new Satelite();
+		assertEquals(sateliteTest.meridiano, 0, 0);
+		assertEquals(sateliteTest.paralelo, 0, 0);
+		assertEquals(sateliteTest.distanciaTierra, 0, 0);
+	}
+	
+	@Test
+	public void testSetPosicion() {
+		meridiano = 11;
+		paralelo = 22;
+		distanciaTierra = 44;
+		Satelite sateliteTest = new Satelite();
+		sateliteTest.setPosicion(meridiano, paralelo, distanciaTierra);
+		assertEquals(sateliteTest.meridiano, 11, 0);
+		assertEquals(sateliteTest.paralelo, 22, 0);
+		assertEquals(sateliteTest.distanciaTierra, 44, 0);
+	}
+}
